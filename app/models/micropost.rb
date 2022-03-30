385b5d0966c17d4f9c5a1dc0eb7745be8ff3f6d6
Rhea <rheaazarraga@gmail.com>
2022-03-29 17:46:24 -0700

    edit show.html erb to display a certain users posts, add 2nd validation so that posts cannot be blank

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 },
+                      presence: true
+  validates :content, length: { minimum: 1 },
+                      presence: true
 end
